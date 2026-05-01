de20a3a16f3502e3ac25e9dd6be6698a01d1c43b
Pcol <mandarincik3000@gmail.com>
2026-05-01 15:08:18 +0300

    Locale

--- Content.Server/Chat/Managers/ChatSanitizationManager.cs
+++ Content.Server/Chat/Managers/ChatSanitizationManager.cs
@@ -14,6 +14,34 @@ public sealed class ChatSanitizationManager : IChatSanitizationManager
 {
     private static readonly (Regex regex, string emoteKey)[] ShorthandToEmote =
     [
+        // Barostation-changes-start:
+        Entry("хд", "chatsan-laughs"),
+        Entry("о-о", "chatsan-wide-eyed"), // cyrillic о
+        Entry("о.о", "chatsan-wide-eyed"), // cyrillic о
+        Entry("0_о", "chatsan-wide-eyed"), // cyrillic о
+        Entry("о/", "chatsan-waves"), // cyrillic о
+        Entry("о7", "chatsan-salutes"), // cyrillic о
+        Entry("0_o", "chatsan-wide-eyed"),
+        Entry("лмао", "chatsan-laughs"),
+        Entry("рофл", "chatsan-laughs"),
+        Entry("яхз", "chatsan-shrugs"),
+        Entry(":0", "chatsan-surprised"),
+        Entry(":ь", "chatsan-stick-out-tongue"), // cyrillic ь
+        Entry("=р", "chatsan-stick-out-tongue"), // cyrillic р
+        Entry("=ь", "chatsan-stick-out-tongue"), //cyrillic ь
+        Entry("кек", "chatsan-laughs"),
+        Entry("T_T", "chatsan-cries"),
+        Entry("Т_Т", "chatsan-cries"), // cyrillic T
+        Entry("=_(", "chatsan-cries"),
+        Entry("!с", "chatsan-laughs"),
+        Entry("!в", "chatsan-sighs"),
+        Entry("!х", "chatsan-claps"),
+        Entry("!щ", "chatsan-snaps"),
+        Entry("))", "chatsan-smiles-widely"),
+        Entry(")", "chatsan-smiles"),
+        Entry("((", "chatsan-frowns-deeply"),
+        Entry("(", "chatsan-frowns"),
+        // Barostation-changes-end.
         Entry(":)", "chatsan-smiles"),
         Entry(":]", "chatsan-smiles"),
         Entry("=)", "chatsan-smiles"),
@@ -63,10 +91,10 @@ public sealed class ChatSanitizationManager : IChatSanitizationManager
         Entry("o-o", "chatsan-wide-eyed"),
         Entry("o.o", "chatsan-wide-eyed"),
         Entry("._.", "chatsan-surprised"),
+        Entry( "!", "chatsan-surprised"),
         Entry(".-.", "chatsan-confused"),
         Entry("-_-", "chatsan-unimpressed"),
         Entry("smh", "chatsan-unimpressed"),
-        Entry(":?", "chatsan-shrugs"),
         Entry("o/", "chatsan-waves"),
         Entry("^^/", "chatsan-waves"),
         Entry(":/", "chatsan-uncertain"),
@@ -79,6 +107,7 @@ public sealed class ChatSanitizationManager : IChatSanitizationManager
         Entry("rofl", "chatsan-laughs"),
         Entry("o7", "chatsan-salutes"),
         Entry(";_;7", "chatsan-tearfully-salutes"),
+        Entry("idk", "chatsan-shrugs"),
         Entry(";)", "chatsan-winks"),
         Entry(";]", "chatsan-winks"),
         Entry("(;", "chatsan-winks"),
